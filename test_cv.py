1d160740b5d6e4bc2b26ecac9b11a53c5f5a24da
Fabian <fabianvanegasr@gmail.com>
2025-12-31 08:30:42 -0500

    salvo imagenes en predict

--- test_cv.py
+++ test_cv.py
@@ -6,8 +6,9 @@ print("OpenCV:", cv2.__version__)
 print("PyTorch:", torch.__version__)
 
 model = YOLO("yolov8n.pt")
-#result = model.predict( source='https://ultralytics.com/images/bus.jpg')
-result = model.predict( source='https://es.vecteezy.com/foto/35712186-ai-generado-un-grupo-de-domestico-mascota-perros-y-gatos-colgando-patas-terminado-repisa')
+result = model.predict( source='https://github.com/fabianvanegasr/proyecto2/blob/main/personas.jpg',
+                       save=True)
+
 print("Parece que funcionó")
 
 print("YOLO listo")
