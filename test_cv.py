f9e03ae7c6c848ad7ccdbecf09066f07d2a87c64
Fabian <fabianvanegasr@gmail.com>
2025-12-31 08:14:00 -0500

    guardado de imagenes en predict

--- test_cv.py
+++ test_cv.py
@@ -7,7 +7,19 @@ print("PyTorch:", torch.__version__)
 
 model = YOLO("yolov8n.pt")
 #result = model.predict( source='https://ultralytics.com/images/bus.jpg')
-result = model.predict( source='https://es.vecteezy.com/foto/35712186-ai-generado-un-grupo-de-domestico-mascota-perros-y-gatos-colgando-patas-terminado-repisa')
+result = model.predict( source='https://github.com/fabianvanegasr/proyecto2/blob/main/personas.jpg')
 print("Parece que funcionó")
 
 print("YOLO listo")
+
+# Obtener la imagen procesada
+# `result[0].plot()` genera una imagen con las predicciones (cuadros delimitadores, etiquetas, etc.)
+# Puedes guardar esa imagen en una carpeta.
+
+output_image = result[0].plot()  # Esto genera la imagen procesada con las predicciones
+
+# Guardar la imagen procesada en la carpeta 'predicciones'
+output_path = 'runs/detect/predict/personas_predicciones.jpg'
+cv2.imwrite(output_path, output_image)
+
+print("Imagen con predicciones guardada en:", output_path)
